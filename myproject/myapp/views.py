07ee56aa2436a734595359885cb170b19d8c8134
sooraj948 <sooraj2.sathish@gmail.com>
2022-01-14 18:27:40 +0530

    Basic seacrh based on company given as url parameter

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -9,16 +9,17 @@ def display(request):
     print("Request is ")
     print(request)
     print(type(request))
+    # print(request.GET["company"])
     text = """<h1>welcome to my app !</h1>"""
-    add_entry("")
-    res = get_entries("")
+    # add_entry("")
+    res = get_entries(request)
     
     return HttpResponse(res)
-    return render(request, "/hello.html", {})
+    # return render(request, "/hello.html", {})
 
 def add_entry(request):
     cl = Coupon_List(
-        company="Swiggy",
+        company="Zomato",
         code = "ABC123",
         expiry = datetime.datetime(2015, 10, 9, 23, 55, 59),
         description = "60% off upto Rs 200. No minimum spend",
@@ -33,11 +34,20 @@ def modify_entry(request):
     pass
 def get_entries(request):
 
-    objects = Coupon_List.objects.all()
-    res ='Printing all Dreamreal entries in the DB : <br>'
-   
+    comp=""
+    if ("company" in request.GET):
+        comp  = request.GET["company"]
+    
+    if (comp!=""):
+        objects = Coupon_List.objects.filter(company = comp)
+    else:
+        objects = Coupon_List.objects.all()
+    
+    res ='Printing all Coupon entries in the DB : <br>'
+    # objects = objects.filter(request.GET["company"])
     for elt in objects:
         res += elt.company + " \t" + elt.code + " \t" + elt.description+"<br>"
+    
 
     return res
 
